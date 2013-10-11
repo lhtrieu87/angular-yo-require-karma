36c0508cbc884790303789d7273276b1379a521a
darksteelgames <darksteelgames@darksteelgamess-Mac-mini.local>
2013-10-11 22:54:04 +0800

    Remove unnecessary files: lib

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -1,7 +1,7 @@
 define(["app", "config"], function(app, config) {
 	return app.config(['$routeProvider', function($routeProvider) {
 		$routeProvider.when("/", {
-			templateUrl: "scripts/partials/home.html"
+			templateUrl: "views/partials/home.html"
 		}).otherwise({
 			redirectTo: '/'
 		})
